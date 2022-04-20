cde12bf2f04234ddce457a2b1c76150a9b212d04
Insany Design <insanydesign@MacBook-Pro-de-Insany.local>
2022-04-20 09:36:18 -0300

    projeto atualizado

--- src/components/CardPokemon/index.tsx
+++ src/components/CardPokemon/index.tsx
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-=======
 import Image from "next/image";
 import { useState } from "react";
 import { Modal } from "../Modal";
->>>>>>> 9d75fe357a56b585bde47c8805d7badd4a5458ea
 import { Card } from "./styles";
 
 interface PokemonProps {
   pokemon: {
     id: number;
     name: string;
-<<<<<<< HEAD
     abilities: any;
     sprites: any;
     stats: any;
@@ -24,86 +18,6 @@ interface PokemonProps {
   handleOpenModal: any;
 }
 
-export function CardPokemon({ pokemon, handleOpenModal }: PokemonProps) {
-  return (
-    <Card>
-      <button
-        className={`btn-pokemon ${pokemon.types[0].type.name}`}
-        onClick={() => handleOpenModal(pokemon.id)}
-      >
-        <div className="image">
-          {pokemon.sprites.other.dream_world.front_default && (
-            <Image
-              src={pokemon.sprites.other.dream_world.front_default}
-              alt="image"
-              width={110}
-              height={110}
-            />
-          )}
-        </div>
-
-        <div className="desc">
-          <div className="left-desc">
-            <span>
-              {pokemon.id < 10
-                ? "#00" + pokemon.id
-                : pokemon.id < 100
-                ? "#0" + pokemon.id
-                : "#" + pokemon.id}
-            </span>
-            <strong>
-              {pokemon.name
-                ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-                : ""}
-            </strong>
-          </div>
-
-          <div className="icon">
-            <Image
-              src={`/assets/${pokemon.types[0].type.name}`.concat(".svg")}
-              width={16}
-              height={16}
-              alt="icone"
-            />
-          </div>
-        </div>
-      </button>
-    </Card>
-=======
-    abilities: [
-      {
-        ability: {
-          name: string;
-        };
-      }
-    ];
-    sprites: {
-      other: {
-        dream_world: {
-          front_default: string;
-        };
-      };
-    };
-    stats: [
-      {
-        base_stat: number;
-        stat: {
-          name: string;
-        };
-      }
-    ];
-    height: number;
-    weight: number;
-    types: [
-      {
-        type: {
-          name: string;
-        };
-      }
-    ];
-  };
-}
-
 export function CardPokemon({ pokemon }: PokemonProps) {
   const [modal, setModal] = useState(false);
   const [detalhes, setDetalhes] = useState<any>();
@@ -111,7 +25,7 @@ export function CardPokemon({ pokemon }: PokemonProps) {
 
   function handleOpenModal(id: number) {
     setModal(true);
-    const pokemon = pokemons.find((poke) => poke.pokemon.id === id);
+    const pokemon = pokemons.find((poke: any) => poke.pokemon.id === id);
 
     // const type = types.find(type => type.id === id);
 
@@ -138,51 +52,47 @@ export function CardPokemon({ pokemon }: PokemonProps) {
   return (
     <>
       <Card>
-        <ul className="grid-pokemon">
-          <li key={pokemon.id}>
-            <button
-              className={`btn-pokemon ${pokemon.types[0].type.name}`}
-              onClick={() => handleOpenModal(pokemon.id)}
-            >
-              <div className="image">
-                {pokemon.sprites.other.dream_world.front_default && (
-                  <Image
-                    src={pokemon.sprites.other.dream_world.front_default}
-                    alt="image"
-                    width={110}
-                    height={110}
-                  />
-                )}
-              </div>
+        <button
+          className={`btn-pokemon ${pokemon.types[0].type.name}`}
+          onClick={() => handleOpenModal(pokemon.id)}
+        >
+          <div className="image">
+            {pokemon.sprites.other.dream_world.front_default && (
+              <Image
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt="image"
+                width={110}
+                height={110}
+              />
+            )}
+          </div>
 
-              <div className="desc">
-                <div className="left-desc">
-                  <span>
-                    {pokemon.id < 10
-                      ? "#00" + pokemon.id
-                      : pokemon.id < 100
-                      ? "#0" + pokemon.id
-                      : "#" + pokemon.id}
-                  </span>
-                  <strong>
-                    {pokemon.name
-                      ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-                      : ""}
-                  </strong>
-                </div>
+          <div className="desc">
+            <div className="left-desc">
+              <span>
+                {pokemon.id < 10
+                  ? "#00" + pokemon.id
+                  : pokemon.id < 100
+                    ? "#0" + pokemon.id
+                    : "#" + pokemon.id}
+              </span>
+              <strong>
+                {pokemon.name
+                  ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+                  : ""}
+              </strong>
+            </div>
 
-                <div className="icon">
-                  <Image
-                    src={`/assets/${pokemon.types[0].type.name}`.concat(".svg")}
-                    width={16}
-                    height={16}
-                    alt="icone"
-                  />
-                </div>
-              </div>
-            </button>
-          </li>
-        </ul>
+            <div className="icon">
+              <Image
+                src={`/assets/${pokemon.types[0].type.name}`.concat(".svg")}
+                width={16}
+                height={16}
+                alt="icone"
+              />
+            </div>
+          </div>
+        </button>
       </Card>
 
       {modal && (
@@ -194,6 +104,5 @@ export function CardPokemon({ pokemon }: PokemonProps) {
         />
       )}
     </>
->>>>>>> 9d75fe357a56b585bde47c8805d7badd4a5458ea
   );
 }
